f594c7793be0fe41eddb0e83ffdaad7f2da0324b
Nikoloz995 <nikoloz.astamidze@gmail.com>
2016-02-16 19:59:52 +0400

    Project Birth

---
+++ Balloons Interactive Book/Assets/BallScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public float Speed;
+
+	void Start () {
+       // gameObject.AddComponent<Rigidbody2D>();
+       // gameObject.GetComponent<Rigidbody2D>().gravityScale = 2;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.Translate(new Vector2(0, 1) * Speed);
+	}
+
+
+    void OnMouseDown()
+    {
+        print(gameObject.name);
+    }
+
+}
---
+++ Balloons Interactive Book/Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> PosList;
+
+	void Start () {
+
+        
+	}
+	
+	void Update () {
+	
+	}
+}
