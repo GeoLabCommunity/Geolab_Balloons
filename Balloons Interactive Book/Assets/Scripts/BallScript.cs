39fcd2aa6aa8c6cd34de13d30aca004e76ed0916
Nikoloz995 <nikoloz.astamidze@gmail.com>
2016-02-23 19:28:08 +0400

    Update_3

---
+++ Balloons Interactive Book/Assets/Scripts/BallScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public float Speed;
+
+    // Tamashis gashvebisas pirveli eshveba punqcia Start (aseve arsebobs funcia Awake romelic Start amdeeshveba, sachiroebis shemtxvevashi iyeneben)
+	void Start () {
+       // gameObject.AddComponent<Rigidbody2D>();
+       // gameObject.GetComponent<Rigidbody2D>().gravityScale = 2;
+
+        int Jami = Random.Range(1, 11);
+
+        int pirveli = Random.Range(0, Jami);
+        int meore = Jami - pirveli;
+        string magaliti = pirveli + "+" + meore;
+
+        gameObject.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = magaliti;
+	
+	}
+	
+	//yovel Framze (kadrze) rac xdeba icereba update-Shi
+	void Update () {
+        // Translate - umatebs trasform.positions imdens ramdensac gadascemt parametrad 
+        gameObject.transform.Translate(new Vector2(0, 1) * Speed);
+	}
+
+    // obieqtze mausit an tapit (telefonit) dajeris dapiqsireba - aucilebelia obieqtze idos raime tipis Collider i rom dapiqsirdes dachera
+    void OnMouseDown()
+    {
+        print(gameObject.name);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Balloons Interactive Book/Assets/Scripts/GameController_1.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameController_1 : MonoBehaviour
+{
+
+    public float Timer;
+    float OldTimer;
+
+    public List<GameObject> PosList;
+    public List<GameObject> Blns;
+    public List<GameObject> Btns;
+    int counter = 3;
+
+	void Start () {
+        // Timer shenaxva , shemdegi ganaxlebistvis
+        OldTimer = Timer;
+
+        //int Jami = Random.Range(1, 10);
+        //int pirveli = Random.Range(0, Jami);
+        //int meore = Jami - pirveli;
+
+        //print(pirveli + " + " + meore + " = " + Jami);
+
+	}
+
+	
+	void Update () {
+
+        if (counter <= 0) return;
+
+        Timer -= Time.deltaTime;
+        if (Timer<=0)
+        {
+            counter--;
+            Instantiate(Blns[Random.Range(0,Blns.Count)], PosList[Random.Range(0,PosList.Count)].transform.position, Quaternion.identity);
+            Timer = OldTimer;
+        }
+	}
+
+    //public void PrintName(Button btn)
+    //{
+    //    print(btn.transform.GetChild(0).gameObject.GetComponent<Text>().text);
+    //}
+}
